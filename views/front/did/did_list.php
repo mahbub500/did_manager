f3465702ae8126370ed0d439c1bfd84646805d32
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-16 07:46:17 +0600

    NId & name added

--- views/front/did/did_list.php
+++ views/front/did/did_list.php
@@ -1,14 +1,16 @@
 <form>
   <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <label for="dm_nid" class="col-sm-2 col-form-label">Nid :</label>
     <div class="col-sm-10">
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+      <input type="number" readonly class="form-control-plaintext" id="dm_nid" placeholder="Enter your Nid" >
+      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+
     </div>
   </div>
   <div class="form-group row">
-    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <label for="dm_name" class="col-sm-2 col-form-label">Name :</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+      <input type="text" class="form-control" id="dm_name" placeholder="Enter your name">
     </div>
   </div>
    <button type="submit" class="btn btn-primary">Submit</button>
