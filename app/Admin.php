bbbf6dda89059eb593041ac22d75ce65cc60c4d6
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-16 08:30:54 +0600

    table creatd

--- app/Admin.php
+++ app/Admin.php
@@ -54,6 +54,34 @@ class Admin extends Base {
 		if( ! get_option( 'did-manager_install_time' ) ){
 			update_option( 'did-manager_install_time', time() );
 		}
+
+		global $wpdb;
+
+
+	    $table_name = $wpdb->prefix . 'did_user_list';
+
+
+	    if( $wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name ) {
+	        
+
+	        $charset_collate = $wpdb->get_charset_collate();
+
+	        $sql = "CREATE TABLE $table_name (
+	            id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+	            added_by BIGINT(20) UNSIGNED NOT NULL,
+	            user_name VARCHAR(255) NOT NULL,
+	            user_nid VARCHAR(100) NOT NULL,
+	            created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	            PRIMARY KEY (id),
+	            KEY user_id (user_id)
+	        ) $charset_collate;";
+
+
+	        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+
+	        dbDelta($sql);
+	    }
 	}
 
 	/**
