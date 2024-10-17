016870a4f8632cfe70759b21d0b58ac59e38f480
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-17 07:47:50 +0600

    js code organize

--- assets/js/front.js
+++ assets/js/front.js
@@ -15,22 +15,21 @@ jQuery(function($){
     function checkLength(inputElement, displayElementId) {
 	    const length = $(inputElement).val().length;
 
-	    if ([ 9, 16].includes(length)) {
-	    	$('#dm_notice').hide();
-	    	$('#dm_submit').attr( 'disabled' );
-	    	$('#dm_submit').removeAttr('disabled');
-	    }else{
-	    	$('#dm_notice').show();
-	        $('#dm_notice').html('Enter 10 or 17 Digit Nid Nunber'); 
-	        $('#dm_notice').addClass( 'text-danger' ); 
+	    if ([9, 16].includes(length)) {
+	        $('#dm_notice').hide();
+	        $('#dm_submit').removeAttr('disabled');
+	    } else {
+	        $('#dm_notice').show()
+	            .html('Enter 10 or 17 Digit Nid Number')
+	            .addClass('text-danger');
 	        $('#dm_submit').attr('disabled', true);
 	    }
 	}
 
+	$('#dm_nid').on('input', function() {
+	    checkLength(this, '#dm_nid');
+	});
 
-    $('#dm_nid').on('keypress', function() {
-        checkLength(this, '#dm_nid');
-    });
 
     $( "#dm_nid" ).on( "blur", function() {
 
