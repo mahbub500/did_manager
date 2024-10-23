291262e4ee5a528c2adbac379560803da5bd131e
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-23 06:36:31 +0600

    in edit modal just show data

--- views/front/did/view_user.php
+++ views/front/did/view_user.php
@@ -92,33 +92,33 @@ $results = $wpdb->get_results("SELECT * FROM $table_name");
             <div class="form-group row">
                 <label for="dm_nid" class="col-sm-2 col-form-label">Nid :</label>
                 <div class="col-sm-10">
-                    <input type="number" name="nid_number" class="form-control-plaintext" id="dm_nid" placeholder="Enter your Nid" >
+                    <input type="number" readonly value="<?php echo esc_html($row->nid_number); ?>" name="nid_number" class="form-control-plaintext" id="dm_nid_edit" placeholder="Enter your Nid" >
                     <small id="dm_notice" class="form-text "></small>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="dm_name" class="col-sm-2 col-form-label">Name :</label>
                 <div class="col-sm-10">
-                    <input type="text" required name="user_name" class="form-control" id="dm_name" placeholder="Enter your name">
+                    <input type="text" value="<?php echo esc_html($row->user_name); ?>" required name="user_name" class="form-control" id="dm_name_edit" placeholder="Enter your name">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="dm_birthday" class="col-sm-2 col-form-label">Birth Day :</label>
                 <div class="col-sm-10">
-                    <input type="date" name="dm_birthday" class="form-control" id="dm_birthday" required>
+                    <input type="date" value="<?php echo esc_html($row->birthday); ?>" name="dm_birthday" class="form-control" id="dm_birthday" required>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="dm_mobile_no" class="col-sm-2 col-form-label">Mobile No :</label>
                 <div class="col-sm-10">
-                    <input type="number" minlength="0" maxlength="10" required name="dm_mobile_no" class="form-control" id="dm_mobile_no">
+                    <input type="number" value="<?php echo esc_html($row->mobile_no); ?>" minlength="0" maxlength="10" required name="dm_mobile_no" class="form-control" id="dm_mobile_no">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="dm_upozila" class="col-sm-2 col-form-label">Upozila :</label>
                 <div class="col-sm-10">
                     <select name='dm_upozila' class="form-control" id="dm_upozila">
-                        <option value="">Select Upozila</option>
+                        <option value="<?php echo esc_html($row->upozila); ?>">Select Upozila</option>
                         <?php 
                         foreach (all_upozila_list() as $key => $value) {
                             $selected = ($key === 'Companiganj') ? 'selected' : '';  
@@ -132,7 +132,7 @@ $results = $wpdb->get_results("SELECT * FROM $table_name");
                 <label for="dm_union" class="col-sm-2 col-form-label">Union :</label>
                 <div class="col-sm-10">
                     <select name='dm_union' class="form-control" id="dm_union">
-                        <option value="">Select Union</option>
+                        <option value="<?php echo esc_html($row->union); ?>">Select Union</option>
                         <?php foreach (all_upozila_list() as $key => $value): ?>
                             <?php foreach ($value as $union): ?>
                                 <option class="union-option" data-upozila="<?php echo $key; ?>" value="<?php echo $union; ?>">
@@ -146,14 +146,15 @@ $results = $wpdb->get_results("SELECT * FROM $table_name");
             <div class="form-group row">
                 <label for="dm_word_no" class="col-sm-2 col-form-label">Word No :</label>
                 <div class="col-sm-10">
-                    <input type="number" name="dm_word_no" class="form-control" id="dm_word_no">
+                    <input type="number" value="<?php echo esc_html($row->user_name); ?>" name="dm_word_no" class="form-control" id="dm_word_no">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="dm_image" class="col-sm-2 col-form-label">Image :</label>
                 <div class="col-sm-10">
-                    <input type="file" name="dm_image" class="form-control image-input" id="dm_image" data-preview="#image_preview" accept="image/*">
-                    <img id="image_preview" src="" alt="Image Preview" style="display:none; max-width: 100px; margin-top: 10px;" />
+                    <input type="file"  name="dm_image" class="form-control image-input" id="dm_image" data-preview="#image_preview" accept="image/*">
+                    <img  id="image_preview" src="" alt="Image Preview" style="display:none; max-width: 100px; margin-top: 10px;" />
+                    <?php echo wp_get_attachment_image($attachment_id, array('50', '50'));  ?>
                 </div>
             </div>
             <div class="form-group row">
@@ -161,6 +162,7 @@ $results = $wpdb->get_results("SELECT * FROM $table_name");
                 <div class="col-sm-10">
                     <input type="file" name="dm_nid_image" class="form-control image-input" id="dm_nid_image" data-preview="#nid_preview" accept="image/*">
                     <img id="nid_preview" src="" alt="NID Preview" style="display:none; max-width: 100px; margin-top: 10px;" />
+                     <?php echo wp_get_attachment_image($nid, array('50', '50'));  ?>
                 </div>
             </div>
             <button id="dm_update" type="submit" disabled class="btn btn-primary">Update</button>
