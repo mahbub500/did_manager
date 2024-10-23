c7a838686ab8b9ea8b40e1fcd0cf948c6f476e4f
Mahbub Rahman <38538845+mahbub500@users.noreply.github.com>
2024-10-23 06:09:49 +0600

    misc

--- views/front/dahsboard.php
+++ views/front/dahsboard.php
@@ -15,20 +15,20 @@
     <div class="col-md-12">
      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
       <li class="nav-item" role="presentation">
-        <button class="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Add User</button>
+        <button class="nav-link active" id="pills-home-tab" data-toggle="pill" data-target="#pills-add-user" type="button" >Add User</button>
       </li>
       <li class="nav-item" role="presentation">
-        <button class="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">View User</button>
+        <button class="nav-link" id="pills-profile-tab" data-toggle="pill" data-target="#pills-view-user" type="button" >View User</button>
       </li>
       <li class="nav-item" role="presentation">
-        <button class="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+        <button class="nav-link" id="pills-contact-tab" data-toggle="pill" data-target="#pills-contact" type="button" >Contact</button>
       </li>
     </ul>
     <div class="tab-content" id="pills-tabContent">
-      <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+      <div class="tab-pane fade show active" id="pills-add-user" role="tabpanel" aria-labelledby="pills-home-tab">
         <?php echo Helper::get_template( 'add_user', '/views/front/did' ); ?>
       </div>
-      <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+      <div class="tab-pane fade" id="pills-view-user" role="tabpanel" aria-labelledby="pills-profile-tab">
          <?php echo Helper::get_template( 'view_user', '/views/front/did' ); ?>
       </div>
       <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
